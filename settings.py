816ec74aba49d6e2fd6684be57b66050457f0b37
alexvi88 <37776143+alexvi88@users.noreply.github.com>
2018-05-20 15:45:24 +0300

    20/05/2018

---
+++ settings.py
@@ -0,0 +1,19 @@
+group_for_photos_id = 'xxx'
+group_for_posting_id = 'xxx'
+
+album_id = 'xxx'
+
+user_access_token = 'xxx'
+
+vk_api_version = 5.75
+
+#in seconds
+posts_interval = 3600
+requests_interval = 0.5
+
+directory_with_photos = 'xxx'
+
+#delete photo from directory after posting or not
+delete_photos_after_posting = True
+
+photo_formats = ['.jpg', '.png', '.jpeg']
