0d93d48bcbe3d4cfe27726b39b4d9a7fce08c0bf
alexvi88 <37776143+alexvi88@users.noreply.github.com>
2018-05-22 15:38:28 +0300

    22/05/2018

--- checking_delay.py
+++ checking_delay.py
@@ -1,5 +1,7 @@
 import argparse
-
+import datetime
+import settings
+from last_post_time import last_postponed_post_time
 
 def delay_of_first_post():
 
@@ -12,11 +14,24 @@ def delay_of_first_post():
                         help='delay of first post in hours',
                         type=int)
 
+    parser.add_argument('--afterall',
+                        help='first one will be posted after all',
+                        action='store_true')
+
     args = parser.parse_args()
 
     need_first_post_delay = (args.time is not None)
+    need_posting_after_all = args.afterall
+
+    if need_posting_after_all:
+        last_postponed_time = int(last_postponed_post_time())
+
+        if last_postponed_time != 0:
+            now_temp_d = datetime.datetime.now()
+            now_date = now_temp_d.timestamp()
+            delay += int(last_postponed_time - now_date) + settings.posts_interval
 
     if need_first_post_delay:
-        delay = int(args.time)
+        delay += int(args.time)*3600
 
     return delay
