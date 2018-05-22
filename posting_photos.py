63808a706f9cd823e15622e8f8147d6ee2255c9a
alexvi88 <37776143+alexvi88@users.noreply.github.com>
2018-05-22 14:37:53 +0300

    22/05/2018

--- posting_photos.py
+++ posting_photos.py
@@ -5,6 +5,10 @@ import time
 from posting_photo_with_delay import post_photo_with_delay
 from uploading_photo_to_album import upload_photo_to_album
 from deleting_photos import delete_photo
+from checking_delay import delay_of_first_post
+
+
+first_post_delay = delay_of_first_post()
 
 all_photos = [file_name
               for file_name in os.listdir(settings.directory_with_photos)
@@ -17,7 +21,7 @@ for photo_name in all_photos:
     photo_parameters = upload_photo_to_album(photo_path)
 
     try:
-        post_photo_with_delay(photo_parameters, cur_delay)
+        post_photo_with_delay(photo_parameters, cur_delay, first_post_delay)
         if settings.delete_photos_after_posting:
             delete_photo(photo_path)
     except Exception as e:
